58ff89327a95b0c535f1b560f4b47d1aa14a00b8
Jason Fleetwood-Boldt <jason@heliosdev.shop>
2023-11-03 13:30:26 -0400

    setup for Heroku

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 web: bundle exec rails server -p $PORT
+release: ./release-tasks.shweb: bundle exec rails server -p $PORT
 release: ./release-tasks.sh
--- release-tasks.sh
+++ release-tasks.sh
@@ -6,4 +6,12 @@ then
   puts '*** RELEASE COMMAND FAILED'
   # something went wrong; convey that and exit
   exit 1
+fi# Step to execute
+bundle exec rails db:migrate data:migrate
+# check for a good exit
+if [ 0 -ne 0 ]
+then
+  puts '*** RELEASE COMMAND FAILED'
+  # something went wrong; convey that and exit
+  exit 1
 fi
